02e328a83ac10208483a3a78e4443d826cdccd82
aSeniyants <kadi4inworld@gmail.com>
2022-04-30 22:41:27 +0300

    Initial Commit

---
+++ my map swiftUI/my_map_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_map_swiftUIApp.swift
+//  my map swiftUI
+//
+//  Created by Аркадий Торвальдс on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct my_map_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
