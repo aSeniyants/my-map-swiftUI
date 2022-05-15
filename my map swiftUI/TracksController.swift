b1f9c5fe28512dd1d3ff98adcfbed454a9d55906
aSeniyants <kadi4inworld@gmail.com>
2022-05-15 23:28:22 +0300

    Начал работать над отрисовкой с помощью пятой кнопки

--- my map swiftUI/TracksController.swift
+++ my map swiftUI/TracksController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Аркадий Торвальдс on 01.05.2022.
 //
-
 import Foundation
 import SwiftUI
 import RealmSwift
@@ -12,43 +11,52 @@ import MapKit
 
 struct TracksController: View {
     let realm: Realm
+    @State var isOn: Bool = false
     
     var body: some View{
         NavigationView{
             VStack {
-                listView()
+                
+                listView
+                
                 Button(action: {
                     print("refresh")
-//                    let trackListTable = realm.objects(trackList.self)
-//                    print(trackListTable.count)
+                    let trackListTable = realm.objects(trackList.self)
+                    print(trackListTable)
                 }) {
-                    Text("Обновить")
+                    Text("Распечатать")
                 }
             }
+        }
     }
-}
-
-    struct listView: View {
-        
-        var body: some View {
-            SwiftUI.List {
-                ForEach(try! Realm().objects(trackList.self) , id: \.self) { elem in
+    
+    private var listView: some View {
+        List {
+            ForEach(realm.objects(trackList.self), id: \.self) { elem in
                     Text(String(elem.coordinates.count))
-                }.onDelete(perform: deleteRow)
-            }
+            }.onDelete(perform: deleteRow)
         }
-                
-        func deleteRow(at offsets: IndexSet) {
-            print(IndexSet.self)
-        }
-        
     }
+    
+//    func doBendingBool(value: Bool) -> Binding<Bool> {
+//        let BendingVar = Binding<Bool>(
+//            get: { value },
+//            set: { $0 }
+//        )
+//        return BendingVar
+//    }
+    
+    private func deleteRow(at offsets: IndexSet) {
+        print(offsets)
+    }
+    
+    
 }
 
 class locationList: Object {
     @objc dynamic var latitude = 0.0
     @objc dynamic var longitude = 0.0
-
+    
     /// Computed properties are ignored in Realm
     var coordinate: CLLocationCoordinate2D {
         return CLLocationCoordinate2D(
@@ -60,4 +68,5 @@ class locationList: Object {
 class trackList: Object {
     let coordinates = RealmSwift.List<locationList>()
     @objc dynamic var nameTrack: String = ""
-    }
+    @objc dynamic var isDraw: Bool = false
+}
